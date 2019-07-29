ba08d9561d0e443cbd649c3406e5f1e7886add8b
tanlizhong <2605715862@qq.com>
2019-07-29 08:08:05 +0800

    后端代码

---
+++ src/com/czxy/demo/test01.java
@@ -0,0 +1,10 @@
+package com.czxy.demo;
+
+public class test01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("将代码提交到本地仓库");
+        System.out.println("将本地仓库提交到远程仓库");
+    }
+}
