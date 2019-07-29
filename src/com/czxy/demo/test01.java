2e84ecc9926b54bb71e1e77bfbf038d338e165e7
tanlizhong <2605715862@qq.com>
2019-07-29 08:26:59 +0800

    后端代码3

--- src/com/czxy/demo/test01.java
+++ src/com/czxy/demo/test01.java
@@ -7,5 +7,6 @@ public class test01 {
         System.out.println("将代码提交到本地仓库");
         System.out.println("将本地仓库提交到远程仓库");
         System.out.println("左边方式提交");
+        System.out.println("左上角方式提交");
     }
 }
